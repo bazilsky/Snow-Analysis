864172964272d92f288bb9617a37773b20d9a8ae
Ananth <77905504+bazilsky@users.noreply.github.com>
2022-04-28 13:47:44 +0100

    seasonal script to filter time values

--- SP_PDF_gamma_FreyACP2019/gamma_blowsea2/gamma_blowsea_mosaic_seasonal.m
+++ SP_PDF_gamma_FreyACP2019/gamma_blowsea2/gamma_blowsea_mosaic_seasonal.m
@@ -20,8 +20,7 @@ DATA = load(fname);
 
 %% select period of interest
 % 1) example BSn at 0.2 & 29m (IS5)
-t1 = datenum('01-Jul-2013 23:15'); 
-t2 = datenum('14-Jul-2013 23:26'); % this statement is not read in 
+ 
 
 % snowfall only #1 observed 3/7/13 15:00, 4/7/13 0:05 & 6:15
 % t1 = datenum('3-Jul-2013 14:30'); t2 = datenum('4-Jul-2013 7:00'); % Nsum of SPC-crw & SPC-ice look similar
@@ -46,110 +45,106 @@ meanT = []
 Nsum_array = []
 uncertainty_of_mean = []
 
-for i = 1:(length(velocity_bins)-1)
+t1 = datenum('01-Jul-2020 00:00'); 
+t2 = datenum('01-Aug-2020 00:00'); % this statement is not read in
 
-    %n = find(DATA.t(:,1)>=t1 & DATA.t(:,1)<t2);
-    n = find(DATA.U8cm_ex(:,1)>=velocity_bins(i) & DATA.U8cm_ex(:,1)<velocity_bins(i+1));
-    num_points = [num_points length(n)];
+% start_date = datenum('01-Jan-2019 00:00');
+% t1 = start_date;
+% t2 = t1+30;
 
-    nonnan_count = nnz(~isnan(DATA.N(n)));
-    uncertainty_of_mean = [uncertainty_of_mean nanstd(DATA.N(n))/sqrt(nonnan_count)]
-    % 2) example diamond dust/ snow crystals
-    % diamond dust #1 observed at 29/07/13 21:38
-    % t1 = datenum('29-Jul-2013 20:00'); t2 = datenum('29-Jul-2013 23:00');
-    % diamond dust #2 observed at 30/07/13 9:15
-    % t1 = datenum('30-Jul-2013 7:30'); t2 = datenum('30-Jul-2013 10:30');
-    %% x-data: 64 SPC particle size bins
-    x = DATA.dp_bins(:,3)'; % dp in micron
-    binWidth = DATA.dp_bins(:,4)'; % binwidth in micron
-    % remove bottom & top bin of spc number densities because spurious particle detection
-    x(1) = []; x(end) = [];
-    binWidth(1) = []; binWidth(end) = [];
 
-%% y-data (pdf) & other
-% average if more than one line
-    N = nanmean(DATA.N(n,:),1);
-    T = nanmean(DATA.T(n,:),1);
-    %dp_mean = nanmean(DATA.dp_mean(n,:),1);
-    %ust = nanmean(DATA.ust(n));
-    %U10m = nanmean(DATA.U10m(n));
-    % remove bot & top bin of spc number densities because spurious particle detection
-    N(:,1) = []; N(:,end) = [];
-    %T(:,1) = []; T(:,end) = [];
-    % compute PDF
-    N_sum = nansum(N,2);
-    pdf = N./(binWidth.*N_sum); 
-    % pdf = N./N_sum; % area=1 but wrong paramters if x_org used
 
-    %% set up data to fit
-    % replace NaN or zero with a small number to ensure stable fit
-    m = find(isnan(pdf) | pdf==0);
-    pdf(m) = 1e-12;
+for t_count=1:1
     
-    %% Fit the best 2P gamma distribution to the noisy data
-    h = @f_2p_gamma_diff;       % name of function for search  
-    data.x = x;                 % structure for the data
-    data.y = pdf;
-    p_init = [2.1 70];         % 1st guess for search [shape scale];
-
-    %% send the information to f_generic_fit
-    % this returns the least square fitted P (p_ret) and the 1-sigma uncertainty in P (ep_ret)
-    [p_ret, ep_ret] = f_generic_fit(data,p_init,h,1);
+    for i = 1:(length(velocity_bins)-1)
     
-    %% Plot best fit to scaled data
-    x_fine = (0:0.01:max(x))';       % nice smooth line
-    pdf_ret = f_build_2p_gamma(x_fine,p_ret);   % calc the curve
+        %n = find(DATA.t(:,1)>=t1 & DATA.t(:,1)<t2);
+        n = find(DATA.U8cm_ex(:,1)>=velocity_bins(i) & DATA.U8cm_ex(:,1)<velocity_bins(i+1)& DATA.t(:,1)>=t1 & DATA.t(:,1)<t2);
+        %n = find(DATA.U8cm_ex(:,1)>=velocity_bins(i) & DATA.U8cm_ex(:,1)<velocity_bins(i+1));
+        num_points = [num_points length(n)];
+    
+        nonnan_count = nnz(~isnan(DATA.N(n)));
+        uncertainty_of_mean = [uncertainty_of_mean nanstd(DATA.N(n))/sqrt(nonnan_count)]
+        % 2) example diamond dust/ snow crystals
+        % diamond dust #1 observed at 29/07/13 21:38
+        % t1 = datenum('29-Jul-2013 20:00'); t2 = datenum('29-Jul-2013 23:00');
+        % diamond dust #2 observed at 30/07/13 9:15
+        % t1 = datenum('30-Jul-2013 7:30'); t2 = datenum('30-Jul-2013 10:30');
+        %% x-data: 64 SPC particle size bins
+        x = DATA.dp_bins(:,3)'; % dp in micron
+        binWidth = DATA.dp_bins(:,4)'; % binwidth in micron
+        % remove bottom & top bin of spc number densities because spurious particle detection
+        x(1) = []; x(end) = [];
+        binWidth(1) = []; binWidth(end) = [];
+    
+    %% y-data (pdf) & other
+    % average if more than one line
+        N = nanmean(DATA.N(n,:),1);
+        T = nanmean(DATA.T(n,:),1);
+        %dp_mean = nanmean(DATA.dp_mean(n,:),1);
+        %ust = nanmean(DATA.ust(n));
+        %U10m = nanmean(DATA.U10m(n));
+        % remove bot & top bin of spc number densities because spurious particle detection
+        N(:,1) = []; N(:,end) = [];
+        %T(:,1) = []; T(:,end) = [];
+        % compute PDF
+        N_sum = nansum(N,2);
+        pdf = N./(binWidth.*N_sum); 
+        % pdf = N./N_sum; % area=1 but wrong paramters if x_org used
+    
+        %% set up data to fit
+        % replace NaN or zero with a small number to ensure stable fit
+        m = find(isnan(pdf) | pdf==0);
+        pdf(m) = 1e-12;
+        
+        %% Fit the best 2P gamma distribution to the noisy data
+        h = @f_2p_gamma_diff;       % name of function for search  
+        data.x = x;                 % structure for the data
+        data.y = pdf;
+        p_init = [2.1 70];         % 1st guess for search [shape scale];
+    
+        %% send the information to f_generic_fit
+        % this returns the least square fitted P (p_ret) and the 1-sigma uncertainty in P (ep_ret)
+        [p_ret, ep_ret] = f_generic_fit(data,p_init,h,1);
+        
+        %% Plot best fit to scaled data
+        x_fine = (0:0.01:max(x))';       % nice smooth line
+        pdf_ret = f_build_2p_gamma(x_fine,p_ret);   % calc the curve
+    
+        Nsum_temp = nansum(DATA.N,2); % this new line because Nsum doesn't exist 
+    
+        Nsum_slice = nansum(Nsum_temp(n))/1e6;
+    
+        %Nsum_slice = nansum(DATA.N_sum(n))/1e6;
+        Nsum_array = [Nsum_array Nsum_slice]
+    
+        
+       % figure(1)   % show it as a bar plot
+        
+        alpha = [alpha p_ret(1)];
+        beta  = [beta  p_ret(2)];
+        meanT = [meanT T]
+        
+        %{
+        figure(1) 
+        str = sprintf('\\alpha %5.2f \\pm %0.2f, \\beta %5.2f \\pm %0.2f, U = %2d-%2d m/s',...
+           p_ret(1), ep_ret(1),p_ret(2), ep_ret(2), velocity_bins(i),velocity_bins(i+1));
+        title('Size distribution for each wind velocity bin','fontsize',20);
+        hold on 
+        %bar(x,pdf)
+        plot(x_fine,pdf_ret,'linewidth',2);
+        xlabel('d_p (\mum)','fontsize',20);
+        ylabel('PDF','fontsize',20);
+        %legend (str)
+        %}
     
 
-    Nsum_temp = nansum(DATA.N,2); % this new line because Nsum doesn't exist 
-
-    Nsum_slice = nansum(Nsum_temp(n))/1e6;
-
-
-    %Nsum_slice = nansum(DATA.N_sum(n))/1e6;
-    Nsum_array = [Nsum_array Nsum_slice]
-
-    figure(1)   % show it as a bar plot
     
+    end
 
-    %str = sprintf('\\alpha %5.2f \\pm %0.2f, \\beta %5.2f \\pm %0.2f, \\alpha\\beta %3.0f \\mum, d_{p} %3.0f \\mum, U = %d-%2d',...
-    %   p_ret(1), ep_ret(1),p_ret(2), ep_ret(2), p_ret(1).*p_ret(2),alpha, velocity_bins(i),velocity_bins(i+1));
-
-    str = sprintf('\\alpha %5.2f \\pm %0.2f, \\beta %5.2f \\pm %0.2f, U = %2d-%2d m/s',...
-       p_ret(1), ep_ret(1),p_ret(2), ep_ret(2), velocity_bins(i),velocity_bins(i+1));
-
-
-    %str2 = sprintf('U = %d','%d', velocity_bins(i), velocity_bins(i+1), ' ')
-
-    %str = strcat(str2, str)
-    
-    %
-    %str_temp = [str_temp ;str]
-    alpha = [alpha p_ret(1)];
-    beta  = [beta  p_ret(2)];
-    meanT = [meanT T]
-    title('Size distribution for each wind velocity bin','fontsize',20);
-    hold on 
-    %bar(x,pdf)
-    plot(x_fine,pdf_ret,'linewidth',2);
-    xlabel('d_p (\mum)','fontsize',20);
-    ylabel('PDF','fontsize',20);
-    %legend (str)
-
-    %{
-    %alpha and beta estimation using xins method
-    totsd = nansum(nansum(DATA.N(n,:).*(DATA.SP_data(n,:) - nanmean(DATA.SP_data(n,:))).^2));
-    beta_1p_temp = totsd/nansum(nansum(DATA.N(n,:)))/nansum(nanmean(DATA.SP_data(n,:)));
-    alpha_1p_temp = nanmean(nanmean(DATA.SP_data(n,:)))/beta_1p_temp;
-    
-    alpha_1p = [alpha_1p alpha_1p_temp]
-    beta_1p  = [beta_1p beta_1p_temp]
-    %}
-
-end
 
 %legend(str_temp)
-
+%{
 figure(2)
 %plot(velocity_bins(1:(length(velocity_bins)-1)), num_points,'b*-','linewidth',2);
 bar(new_v_vector, num_points);
@@ -174,6 +169,7 @@ plot(new_v_vector,alpha.*beta, 'r*-','linewidth', 3)
 xlabel('Velocity bins (m/s)','fontsize',18)
 ylabel('alpha*beta', 'fontsize', 18)
 title('Mean diameter (microns) vs windspeed', 'fontsize', 20)
+%}
 
 poly_order = 3;
 alpha_error = zeros(poly_order,2);
@@ -181,10 +177,11 @@ beta_error = zeros(poly_order,2);
 all_error = zeros(poly_order,4);
 % polynomial fit to data 
 for k=1:poly_order
+    
     velocity_fit_data = new_v_vector(1:2:end);
     alpha_fit_data = alpha(1:2:end);
     beta_fit_data = beta(1:2:end);
-
+    
     velocity_test_data = new_v_vector(2:2:end);
     alpha_test_data = alpha(2:2:end);
     beta_test_data = beta(2:2:end);
@@ -213,9 +210,10 @@ for k=1:poly_order
 end
 
 all_error = [alpha_error beta_error]
+%{
 writematrix(all_error,'mosaic_alphabeta_error.csv')
 
-%{ 
+ 
 
 
 % This is the original working code
@@ -230,7 +228,7 @@ beta_fit_val = polyval(beta_poly,xtemp);
 
 %}
 
-
+%{
 figure(5)
 yyaxis left
 plot(new_v_vector,alpha, 'r*','linewidth', 3)
@@ -282,12 +280,14 @@ yyaxis right
 plot(new_v_vector,num_points,'r*-','linewidth',2)
 ylabel('Number of data points','fontsize',20)
 title('Mean Diamter (\mum) vs Surface windspeed (m/s)','fontsize',18)
+%}
 
-figure(9)
+
 
 plot(new_v_vector,alpha.*beta,'k.-','linewidth',1.5)
 hold on 
 scatter(new_v_vector,alpha.*beta,100,num_points,'filled')
+
 xlabel('U8cm (m/s)','fontsize',20)
 %set(gca,'YScale','log')
 ylabel('Mean Diameter (\mum)','fontsize',20)
@@ -295,8 +295,13 @@ title('Mean Diamter (\mum) vs Surface windspeed (m/s)','fontsize',18)
 hcb = colorbar
 hcb.Title.String = "Number of data points";
 hcb.FontSize = 12
+hold on
 
+t1 = t2
+t2 = t2+60
 
+end 
+%{
 %finding when the campaign happened 
 
 a1 = datestr(DATA.t, 'mm/dd/YYYY');
@@ -307,7 +312,7 @@ end
 figure(10)
 plot(store_month,store_month)
 
-%{
+
 figure(7)
 t_series = datenum(DATA.t);
 t_org = datetime(t_series,'ConvertFrom','datenum');
